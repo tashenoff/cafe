6a95b13d5df1c62584997a9613e88020885148b7
tashenoff <kail.dead.kd@gmail.com>
2025-09-04 12:18:02 +0600

    .

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        accent: '#ff60a6',
+      },
+    },
+  },
+  plugins: [],
+};
+
